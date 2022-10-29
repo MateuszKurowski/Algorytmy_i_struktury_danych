a59bc687aebc17121c1716d6fb3bba3584df48a2
KaMeR <kurowskimateusz04@gmail.com>
2022-10-29 20:11:07 +0200

    lab_1 - INOUTEST - Enormous Input and Output Test
    1. Using Console.ReadLine() and Console.WriteLine()

---
+++ Lab/Lab1-Efektywnosc_algorytmow/INOUTEST-Enormous_Input_and_Output_Test/INOUTEST-Enormous_Input_and_Output_Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace INOUTEST_Enormous_Input_and_Output_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                var numbers = Console.ReadLine().Split(' ');
+                var a = int.Parse(numbers[0]);
+                var b = int.Parse(numbers[1]);
+                result += (a * b) + Environment.NewLine;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
