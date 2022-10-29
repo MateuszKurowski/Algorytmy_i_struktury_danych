ab2c1cb17c4db501d7e8f314d905ddf1ef7600ac
KaMeR <kurowskimateusz04@gmail.com>
2022-10-29 19:18:54 +0200

    Lab_1 - INTEST - Enormous Input Test
    1. Using Console.ReadLine() and Console.WriteLine()

---
+++ Lab/Lab1-Efektywnosc_algorytmow/INTEST-Enormous_input-test/INTEST-Enormous_input-test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace INTEST_Enormous_Input_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstInputArray = Console.ReadLine().Split(' ');
+            var n = int.Parse(firstInputArray[0]);
+            var k = int.Parse(firstInputArray[1]);
+            var result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var t = int.Parse(Console.ReadLine());
+                if (t % k == 0)
+                    result++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
