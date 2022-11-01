1dde630f44cde39d8e9ffa198aaf3443139a7d50
KaMeR <kurowskimateusz04@gmail.com>
2022-11-01 16:43:37 +0100

    lab_2 - CodeRunner
    Done

---
+++ Lab/Lab2-Metoda_Window_Sliding/CodeRunner/CodeRunner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodeRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 4, 2, 10, 23, 3, 1, 0, 20 };
+            var n = 9;
+            var k = 4;
+            Console.WriteLine(MaxSum(arr, n, k));
+        }
+
+        static int MaxSum(int[] arr, int n, int k)
+        {
+            if (k > n)
+                return -1;
+
+            var result = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                result += arr[i];
+            }
+
+            var sum = result;
+            for (int i = k; i < n; i++)
+            {
+                sum += arr[i] - arr[i - k];
+
+                if (sum > result)
+                    result = sum;
+            }
+
+            return result;
+        }
+    }
+}
