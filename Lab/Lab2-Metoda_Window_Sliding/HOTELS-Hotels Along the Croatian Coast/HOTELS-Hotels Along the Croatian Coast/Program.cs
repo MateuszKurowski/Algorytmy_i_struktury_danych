e043e36d717f7536918e59dea9da469455405a6f
KaMeR <kurowskimateusz04@gmail.com>
2022-11-02 17:17:35 +0100

    lab_2 HOTELS - Hotels Along the Croatian CoastZadanie
    Done

---
+++ Lab/Lab2-Metoda_Window_Sliding/HOTELS-Hotels Along the Croatian Coast/HOTELS-Hotels Along the Croatian Coast/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        var firstLine = Console.ReadLine();
+        var secondLine = Console.ReadLine();
+
+        var firstLineArray = Array.ConvertAll(firstLine.Split(' ', StringSplitOptions.RemoveEmptyEntries), s => Convert.ToInt32(s));
+        var n = firstLineArray[0];
+        var m = firstLineArray[1];
+        var hotelsCost = Array.ConvertAll(secondLine.Split(' ', StringSplitOptions.RemoveEmptyEntries), s => Convert.ToInt32(s));
+
+        var maxValue = 0;
+        var firstElementIndex = 0;
+        var currentValue = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (hotelsCost[i] + currentValue <= m)
+            {
+                currentValue += hotelsCost[i];
+            }
+            else
+            {
+                while (hotelsCost[i] + currentValue > m)
+                {
+                    currentValue -= hotelsCost[firstElementIndex];
+                    firstElementIndex++;
+                }
+                currentValue += hotelsCost[i];
+            }
+            if (currentValue > maxValue)
+                maxValue = currentValue;
+        }
+        Console.WriteLine(maxValue);
+    }
+}
