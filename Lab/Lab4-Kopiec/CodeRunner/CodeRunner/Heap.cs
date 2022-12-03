5c978e30b68f648b5633a1e1903d7f7f5cedd08a
KaMeR <kurowskimateusz04@gmail.com>
2022-12-03 23:24:56 +0100

    Lab 4 - CodeRunner: Kopiec

--- Lab/Lab4-Kopiec/CodeRunner/CodeRunner/Heap.cs
+++ Lab/Lab4-Kopiec/CodeRunner/CodeRunner/Heap.cs
@@ -3,9 +3,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class Heap<T> : IEnumerable<T> where T : IComparable<T>
 {
@@ -21,57 +18,130 @@ public class Heap<T> : IEnumerable<T> where T : IComparable<T>
     public Heap(IEnumerable<T> collection, HeapOptions option = HeapOptions.MinHeap)
     {
         this.Option = option;
-        list = new List<T>(collection);
-        switch (option)
+        list = new List<T>();
+        foreach (var item in collection)
         {
-            case HeapOptions.MaxHeap:
-                list.Sort((a, b) => b.CompareTo(a));
-                break;
-
-            case HeapOptions.MinHeap:
-                list.Sort((a, b) => a.CompareTo(b));
-                break;
+            Insert(item);
         }
     }
 
     public int Count => list.Count;
 
+    private void CheckParent(int index)
+    {
+        var parentIndex = (index - 1) / 2;
+        switch (Option)
+        {
+            case HeapOptions.MaxHeap:
+                while (list[index].CompareTo(list[parentIndex]) > 0)
+                {
+                    if (parentIndex > Count - 1 || parentIndex < 0)
+                        break;
+
+                    var temp = list[parentIndex];
+                    list[parentIndex] = list[index];
+                    list[index] = temp;
+                    index = parentIndex;
+                    parentIndex = (index - 1) / 2;
+                }
+                break;
+
+            case HeapOptions.MinHeap:
+                while (list[index].CompareTo(list[parentIndex]) < 0)
+                {
+                    if (parentIndex > Count - 1 || parentIndex < 0)
+                        break;
+
+                    var temp = list[parentIndex];
+                    list[parentIndex] = list[index];
+                    list[index] = temp;
+                    index = parentIndex;
+                    parentIndex = (index - 1) / 2;
+                }
+                break;
+        }
+    }
+
+    private void CheckChild(int index)
+    {
+        var firstChildIndex = (2 * index) + 1;
+        var secondChildIndex = (2 * index) + 2;
+
+        switch (Option)
+        {
+            case HeapOptions.MaxHeap:
+                var biggerIndex = list[firstChildIndex].CompareTo(list[secondChildIndex]) > 0 ? firstChildIndex : secondChildIndex;
+                while (list[index].CompareTo(list[biggerIndex]) < 0)
+                {
+                    T temp = list[biggerIndex];
+                    list[biggerIndex] = list[index];
+                    list[index] = temp;
+                    index = biggerIndex;
+
+                    firstChildIndex = (2 * index) + 1;
+                    secondChildIndex = (2 * index) + 2;
+                    if (firstChildIndex > Count - 1 || secondChildIndex > Count - 1)
+                        break;
+                    biggerIndex = list[firstChildIndex].CompareTo(list[secondChildIndex]) > 0 ? firstChildIndex : secondChildIndex;
+                }
+                break;
+
+            case HeapOptions.MinHeap:
+                var smallerIndex = list[firstChildIndex].CompareTo(list[secondChildIndex]) < 0 ? firstChildIndex : secondChildIndex;
+                while (list[index].CompareTo(list[smallerIndex]) > 0)
+                {
+                    var temp = list[smallerIndex];
+                    list[smallerIndex] = list[index];
+                    list[index] = temp;
+                    index = smallerIndex;
+
+                    firstChildIndex = (2 * index) + 1;
+                    secondChildIndex = (2 * index) + 2;
+                    if (firstChildIndex > Count - 1 || secondChildIndex > Count - 1)
+                        break;
+                    smallerIndex = list[firstChildIndex].CompareTo(list[secondChildIndex]) < 0 ? firstChildIndex : secondChildIndex;
+                }
+                break;
+        }
+    }
+
+
     public void Insert(T x)
     {
         list.Add(x);
-
-        throw new NotImplementedException();
+        var index = list.LastIndexOf(x);
+        CheckParent(index);
     }
 
     public T Delete()
     {
-        throw new NotImplementedException();
+        var elementToDelete = Top();
+        var lastIndex = list.Count - 1;
+        var lastElement = list[lastIndex];
+
+        var temp = lastElement;
+        list[lastIndex] = elementToDelete;
+        list[0] = temp;
+        list.RemoveAt(lastIndex);
+
+        CheckChild(0);
+        
+        return elementToDelete;
     }
 
-    public T Top()
-    {
-        throw new NotImplementedException();
-    }
+    public T Top() => list.Count > 0 ? list[0] : throw new InvalidOperationException("Heap is empty.");
 
-    public void Clear()
-    {
-        throw new NotImplementedException();
-    }
+    public void Clear() => list.Clear();
 
-    public T[] ToArray()
-    {
-        throw new NotImplementedException();
-    }
+    public T[] ToArray() => list.ToArray();
 
     public IEnumerator<T> GetEnumerator()
     {
-        throw new NotImplementedException();
+        foreach (var item in list)
+        {
+            yield return item;
+        }
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-
-    // TODO: próba odczytu elementu lub usunięcia elementu topowego z kopca pustego powoduje zgłoszenie wyjątku InvalidOperationException.
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
--- Lab/Lab4-Kopiec/CodeRunner/CodeRunner/Program.cs
+++ Lab/Lab4-Kopiec/CodeRunner/CodeRunner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodeRunner
 {
@@ -6,7 +7,111 @@ namespace CodeRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test1();
+            WaitForRespond();
+            Test2();
+            WaitForRespond();
+            Test3();
+            WaitForRespond();
+            Test4();
+            WaitForRespond();
+            Test5();
+            WaitForRespond();
+        }
+
+        static void Test1()
+        {
+            var heap = new Heap<int>(new int[] { 2, 1, 6, 7, 1, 3 }, HeapOptions.MaxHeap);
+            int[] tab = heap.ToArray();
+            Console.WriteLine(string.Join(' ', tab));
+            Console.WriteLine("Element szczytowy: " + heap.Top());
+            Console.WriteLine("Liczba elementów: " + heap.Count);
+            heap.Clear();
+            Console.WriteLine("Liczba elementów po czyszczeniu: " + heap.Count);
+            try
+            {
+                Console.WriteLine(heap.Top());
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Wyjątek przechwycony");
+            }
+
+
+            PrintCorrectAnswer("7 6 3 1 1 2\r\nElement szczytowy: 7\r\nLiczba elementów: 6\r\nLiczba elementów po czyszczeniu: 0\r\nWyjątek przechwycony");
+        }
+
+        static void Test2()
+        {
+            // MinHeap, tworzenie kopca
+            var heap = new Heap<int>(new int[] { 2, 1, 6, 7, 1, 3 }, HeapOptions.MinHeap);
+            int[] tab = heap.ToArray();
+            Console.WriteLine(string.Join(' ', tab));
+            Console.WriteLine(heap.Top());
+            heap.Clear();
+            try
+            {
+                Console.WriteLine(heap.Delete());
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Wyjątek przechwycony");
+            }
+
+
+            PrintCorrectAnswer("1 1 3 7 2 6\r\n1\r\nWyjątek przechwycony");
+        }
+
+        static void Test3()
+        {
+            // dodawanie elementu do kopca
+            var heap = new Heap<int>(new int[] { 2, 3, 6, 7, 8, 3 }, HeapOptions.MinHeap);
+            Console.WriteLine(string.Join(' ', heap.ToArray()));
+            heap.Insert(5);
+            Console.WriteLine(string.Join(' ', heap.ToArray()));
+            heap.Insert(1);
+            Console.WriteLine(string.Join(' ', heap.ToArray()));
+
+
+            PrintCorrectAnswer("2 3 3 7 8 6\r\n2 3 3 7 8 6 5\r\n1 2 3 3 8 6 5 7");
+        }
+
+        static void Test4()
+        {
+            // usuwanie elementu z kopca
+            var heap = new Heap<char>(new char[] { 'd', 'f', 'g', 'o', 'c', 'h' }, HeapOptions.MaxHeap);
+            Console.WriteLine(string.Join(' ', heap.ToArray()));
+            Console.WriteLine(heap.Delete());
+            Console.WriteLine(string.Join(' ', heap.ToArray()));
+
+
+            PrintCorrectAnswer("o g h d c f\r\no\r\nh g f d c");
+        }
+
+        static void Test5()
+        {
+            // IEnumerable<T>
+            var heap = new Heap<int>(new List<int> { 2, 1, 6, 7, 1, 3 }, HeapOptions.MaxHeap);
+            foreach (var x in heap)
+                Console.Write(x);
+
+
+            PrintCorrectAnswer("763112");
+        }
+
+        static void WaitForRespond()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void PrintCorrectAnswer(string answer)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Poprawne odpowiedzi: ");
+            Console.WriteLine();
+            Console.WriteLine(answer);
         }
     }
 }
