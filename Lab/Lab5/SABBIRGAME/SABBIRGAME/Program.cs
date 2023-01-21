f6f04f9a9a547fafd76edd4a9aafac4d06db3039
KaMeR <kurowskimateusz04@gmail.com>
2023-01-21 22:44:17 +0100

    SABBIRGAME

---
+++ Lab/Lab5/SABBIRGAME/SABBIRGAME/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SABBIRGAME
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var numberOfCases = int.Parse(Console.ReadLine());
+            for (var i = 0; i < numberOfCases; i++)
+            {
+                var minHP = default(long);
+                var currentHP = default(long);
+                var numberOfLevels = int.Parse(Console.ReadLine());
+                var HPsForLevel = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (var j = 0; j < numberOfLevels; j++)
+                {
+                    currentHP += long.Parse(HPsForLevel[j]);
+                    if (currentHP < 1)
+                    {
+                        minHP += 1 - currentHP;
+                        currentHP = 1;
+                    }
+                }
+                Console.WriteLine(minHP);
+            }
+        }
+    }
+}
