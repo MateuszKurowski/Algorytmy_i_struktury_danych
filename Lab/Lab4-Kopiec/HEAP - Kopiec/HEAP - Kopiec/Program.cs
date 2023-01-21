7a7109961f98c50280f191675a0d96e281316dd5
KaMeR <kurowskimateusz04@gmail.com>
2023-01-21 22:26:02 +0100

    Lab5

--- Lab/Lab4-Kopiec/HEAP - Kopiec/HEAP - Kopiec/Program.cs
+++ Lab/Lab4-Kopiec/HEAP - Kopiec/HEAP - Kopiec/Program.cs
@@ -6,75 +6,59 @@ using System.Text;
 
 internal class Test
 {
-    static void Main()
+    private static void Main()
     {
         var t = FastIO.ReadNonNegativeInt();
-        var sb = new StringBuilder();
+        StringBuilder sb = new();
 
-        for (int i = 0; i < t; i++)
+        for (var i = 0; i < t; i++)
         {
             var input = FastIO.ReadASCIIString().Split(' ', StringSplitOptions.RemoveEmptyEntries);
             var mainJobs = FastIO.ReadASCIIString().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             var n = int.Parse(input[0]);
             var m = int.Parse(input[1]);
             var time = 0;
-            var jobsList = new List<int>(mainJobs);
+            List<int> jobsList = new(mainJobs);
 
             while (jobsList.Count > 0)
             {
                 var job = jobsList[0];
                 jobsList.RemoveAt(0);
                 if (jobsList.Any(x => x > job))
+                {
                     jobsList.Add(job);
+                }
                 else
                 {
                     time++;
                     if (m == 0)
+                    {
                         break;
+                    }
                 }
+
                 m--;
 
                 if (m < 0)
                     m = jobsList.Count - 1;
             }
-            sb.AppendLine(time.ToString());
+
+            _ = sb.AppendLine(time.ToString());
         }
+
         FastIO.WriteASCIIString(sb.ToString());
     }
 }
 
 #region FastIO
+
 public static class FastIO
 {
-    private const byte NULL = (byte)'\0';
-    private const byte NEWLINE = (byte)'\n';
-    private const byte MINUS_SIGN = (byte)'-';
-    private const byte SPACE_CHAR = (byte)' ';
-    private const byte ZERO = (byte)'0';
-    private const int INPUT_BUFFER_LIMIT = 8192;
-    private const int OUTPUT_BUFFER_LIMIT = 8192;
-
-    private static readonly Stream _inputStream = Console.OpenStandardInput();
-    private static readonly byte[] _inputBuffer = new byte[INPUT_BUFFER_LIMIT];
-    private static int _inputBufferSize = 0;
-    private static int _inputBufferIndex = 0;
-
-    private static readonly Stream _outputStream = Console.OpenStandardOutput();
-    private static readonly byte[] _outputBuffer = new byte[OUTPUT_BUFFER_LIMIT];
-    private static readonly byte[] _digitsBuffer = new byte[11];
-    private static int _outputBufferSize = 0;
-
-    private static byte ReadByte()
+    public static void Flush()
     {
-        if (_inputBufferIndex == _inputBufferSize)
-        {
-            _inputBufferIndex = 0;
-            _inputBufferSize = _inputStream.Read(_inputBuffer, 0, INPUT_BUFFER_LIMIT);
-            if (_inputBufferSize == 0)
-                return NULL; // All input has been read.
-        }
-
-        return _inputBuffer[_inputBufferIndex++];
+        _outputStream.Write(_outputBuffer, 0, _outputBufferSize);
+        _outputBufferSize = 0;
+        _outputStream.Flush();
     }
 
     public static string ReadASCIIString(int initialLength = 1)
@@ -88,25 +72,52 @@ public static class FastIO
         while (asciiChar < SPACE_CHAR);
 
         // Build up the string from its chars, until we run into whitespace or the null byte.
-        StringBuilder sb = new StringBuilder(initialLength);
-        sb.Append((char)asciiChar);
+        StringBuilder sb = new(initialLength);
+        _ = sb.Append((char)asciiChar);
         while (true)
         {
             asciiChar = ReadByte();
-            if (asciiChar < SPACE_CHAR) break;
-            sb.Append((char)asciiChar);
+            if (asciiChar < SPACE_CHAR)
+            {
+                break;
+            }
+
+            _ = sb.Append((char)asciiChar);
         }
 
         return sb.ToString();
     }
 
-    public static void WriteASCIIString(string asciiString)
+    public static int ReadInt()
     {
-        Console.Write(asciiString);
-    }
-    public static void WriteLineASCIIString(string asciiString)
-    {
-        Console.WriteLine(asciiString);
+        // Consume and discard whitespace characters (their ASCII codes are all < MINUS_SIGN).
+        byte digit;
+        do
+        {
+            digit = ReadByte();
+        }
+        while (digit < MINUS_SIGN);
+
+        var isNegative = digit == MINUS_SIGN;
+        if (isNegative)
+        {
+            digit = ReadByte();
+        }
+
+        // Build up the integer from its digits, until we run into whitespace or the null byte.
+        var result = isNegative ? -(digit - ZERO) : (digit - ZERO);
+        while (true)
+        {
+            digit = ReadByte();
+            if (digit < ZERO)
+            {
+                break;
+            }
+
+            result = (result * 10) + (isNegative ? -(digit - ZERO) : (digit - ZERO));
+        }
+
+        return result;
     }
 
     public static int ReadNonNegativeInt()
@@ -121,75 +132,31 @@ public static class FastIO
         while (digit < MINUS_SIGN);
 
         // Build up the integer from its digits, until we run into whitespace or the null byte.
-        int result = (digit - ZERO);
+        var result = digit - ZERO;
         while (true)
         {
             digit = ReadByte();
-            if (digit < ZERO) break;
-            result = result * 10 + (digit - ZERO);
+            if (digit < ZERO)
+            {
+                break;
+            }
+
+            result = (result * 10) + (digit - ZERO);
         }
 
         return result;
     }
 
-    public static int ReadInt()
-    {
-        // Consume and discard whitespace characters (their ASCII codes are all < MINUS_SIGN).
-        byte digit;
-        do
-        {
-            digit = ReadByte();
-        }
-        while (digit < MINUS_SIGN);
-
-        bool isNegative = (digit == MINUS_SIGN);
-        if (isNegative)
-        {
-            digit = ReadByte();
-        }
-
-        // Build up the integer from its digits, until we run into whitespace or the null byte.
-        int result = isNegative ? -(digit - ZERO) : (digit - ZERO);
-        while (true)
-        {
-            digit = ReadByte();
-            if (digit < ZERO) break;
-            result = result * 10 + (isNegative ? -(digit - ZERO) : (digit - ZERO));
-        }
-
-        return result;
-    }
-
-    public static void WriteNonNegativeInt(int value)
-    {
-        int digitCount = 0;
-        do
-        {
-            int digit = value % 10;
-            _digitsBuffer[digitCount++] = (byte)(digit + ZERO);
-            value /= 10;
-        } while (value > 0);
-
-        if (_outputBufferSize + digitCount > OUTPUT_BUFFER_LIMIT)
-        {
-            _outputStream.Write(_outputBuffer, 0, _outputBufferSize);
-            _outputBufferSize = 0;
-        }
-
-        while (digitCount > 0)
-        {
-            _outputBuffer[_outputBufferSize++] = _digitsBuffer[--digitCount];
-        }
-    }
+    public static void WriteASCIIString(string asciiString) => Console.Write(asciiString);
 
     public static void WriteInt(int value)
     {
-        bool isNegative = value < 0;
+        var isNegative = value < 0;
 
-        int digitCount = 0;
+        var digitCount = 0;
         do
         {
-            int digit = isNegative ? -(value % 10) : (value % 10);
+            var digit = isNegative ? -(value % 10) : (value % 10);
             _digitsBuffer[digitCount++] = (byte)(digit + ZERO);
             value /= 10;
         } while (value != 0);
@@ -222,11 +189,60 @@ public static class FastIO
         _outputBuffer[_outputBufferSize++] = NEWLINE;
     }
 
-    public static void Flush()
+    public static void WriteLineASCIIString(string asciiString) => Console.WriteLine(asciiString);
+
+    public static void WriteNonNegativeInt(int value)
     {
-        _outputStream.Write(_outputBuffer, 0, _outputBufferSize);
-        _outputBufferSize = 0;
-        _outputStream.Flush();
+        var digitCount = 0;
+        do
+        {
+            var digit = value % 10;
+            _digitsBuffer[digitCount++] = (byte)(digit + ZERO);
+            value /= 10;
+        } while (value > 0);
+
+        if (_outputBufferSize + digitCount > OUTPUT_BUFFER_LIMIT)
+        {
+            _outputStream.Write(_outputBuffer, 0, _outputBufferSize);
+            _outputBufferSize = 0;
+        }
+
+        while (digitCount > 0)
+        {
+            _outputBuffer[_outputBufferSize++] = _digitsBuffer[--digitCount];
+        }
+    }
+
+    private const int INPUT_BUFFER_LIMIT = 8192;
+    private const byte MINUS_SIGN = (byte)'-';
+    private const byte NEWLINE = (byte)'\n';
+    private const byte NULL = (byte)'\0';
+    private const int OUTPUT_BUFFER_LIMIT = 8192;
+    private const byte SPACE_CHAR = (byte)' ';
+    private const byte ZERO = (byte)'0';
+    private static readonly byte[] _digitsBuffer = new byte[11];
+    private static readonly byte[] _inputBuffer = new byte[INPUT_BUFFER_LIMIT];
+    private static readonly Stream _inputStream = Console.OpenStandardInput();
+    private static readonly byte[] _outputBuffer = new byte[OUTPUT_BUFFER_LIMIT];
+    private static readonly Stream _outputStream = Console.OpenStandardOutput();
+    private static int _inputBufferIndex = 0;
+    private static int _inputBufferSize = 0;
+    private static int _outputBufferSize = 0;
+
+    private static byte ReadByte()
+    {
+        if (_inputBufferIndex == _inputBufferSize)
+        {
+            _inputBufferIndex = 0;
+            _inputBufferSize = _inputStream.Read(_inputBuffer, 0, INPUT_BUFFER_LIMIT);
+            if (_inputBufferSize == 0)
+            {
+                return NULL; // All input has been read.
+            }
+        }
+
+        return _inputBuffer[_inputBufferIndex++];
     }
 }
-#endregion
+
+#endregion FastIO
---
+++ Lab/Lab5/Sortowanie_babelkowe/Sortowanie_babelkowe/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sortowanie_babelkowe
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstInput = Console.ReadLine();
+            var secondInput = Console.ReadLine();
+
+            var array = firstInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                            .Select(x => int.Parse(x))
+                                            .ToList();
+            var rangeArray = secondInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(x => int.Parse(x))
+                                                .ToList();
+            if (array.Count < 1
+                    || rangeArray.Count < 1)
+                return;
+
+            var firstIndex = rangeArray[0];
+            var lastIndex = rangeArray[1];
+
+            for (var i = firstIndex; i <= lastIndex; i++)
+            {
+                if (i - 1 < 0) continue;
+                if (array[i] <= array[i - 1])
+                    continue;
+
+                var currentIndex = i;
+                while (array[currentIndex] > array[currentIndex - 1])
+                {
+                    if (currentIndex - 1 < firstIndex)
+                        break;
+                    SwapWithPreviousIndex(array, currentIndex);
+                    currentIndex--;
+                    if (currentIndex <= 0) break;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        private static void SwapWithNextIndex(List<int> array, int index)
+        {
+            if (index + 1 > array.Count + 1) return;
+            var temp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = temp;
+        }
+
+        private static void SwapWithPreviousIndex(List<int> array, int index)
+        {
+            if (index - 1 > array.Count - 1) return;
+            var temp = array[index];
+            array[index] = array[index - 1];
+            array[index - 1] = temp;
+        }
+    }
+}
